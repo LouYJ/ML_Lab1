13ecce77a5549f037d5811c2da2776469ad2d6c7
Yujing Lou <865227010@qq.com>
2016-10-18 20:02:05 +0800

    Mod version3.2

--- version3.py
+++ version3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # coding=utf-8
-#此版本加入了梯度下降优化
+#此版本使用了梯度下降优化
 
 import numpy
 import random
@@ -10,25 +10,22 @@ import math
 fig = pt.figure()
 ax = fig.add_subplot(111)
 
-#定义函数阶数
-order = 9
-#定义步长a
-a=0.01
-#极小值e
-e=1e-3
-#定义参数向量theta
-theta=[]
-#定义梯度向量
-delta=[]
+
+order = 4#定义函数阶数
+a=0.01#定义步长a
+e=1e-3#极小值e
+theta=[]#定义权值向量theta
+delta=[]#定义梯度向量
 
 for i in range(0,order+1):
 	theta.append(0.0)#初始化所有参数均为0
 	delta.append(0.0)#初始化梯度向量
-theta = 200*numpy.random.random(size=order+1)-100#-100,100
+
+theta = 200*numpy.random.random(size=order+1)-100#自动生成一组theta，范围介于-100到100
 theta=numpy.array(theta)
 delta=numpy.array(delta)
 
-
+#函数功能：加入高斯噪声
 def addGaussian(num, s, sigma):
 	mu = 0
 	for i in range(0,len(s)):
@@ -36,19 +33,23 @@ def addGaussian(num, s, sigma):
 		s[i] = s[i] + offset_Guassian
 	return s
 
-def genData(num=20, xrange=1, yrange=1, sigma=0.12):
+#函数功能：用于生成样本数据
+def generateData(num, xrange=1, yrange=1, sigma=0.12):
 	interv = xrange*1.0/num
-	t = numpy.arange(0, xrange, interv)
-	s = yrange*numpy.sin(2*numpy.pi*t)
-	s = addGaussian(s.shape[0], s, sigma)
-	return t, s
+	x = numpy.arange(0, xrange, interv)
+	t = yrange*numpy.sin(2*numpy.pi*x)
+	y = yrange*numpy.sin(2*numpy.pi*x)
+	y = addGaussian(y.shape[0], y, sigma)
+	return x, y, t
 	
+#函数功能：估计函数
 def estimateFun(x,theta,order):
 	sum=0.0
 	for i in range(0,order+1):
 		sum+=theta[i]*numpy.power(x,i)
 	return sum
 	
+#函数功能：损失函数
 def lossFun(X,Y,theta,order):
 	sum=0.0
 	for i in range(0,len(X)):
@@ -58,13 +59,10 @@ def lossFun(X,Y,theta,order):
 		sum+=value
 	return sum
 	
-		
-	
+#函数功能：更新权值参数theta
 def updateTheta(theta,delta,X,Y,order,a):
-#	print theta
 	for i in range(0,order+1):
 		sum=0.0
-#		print 'in'
 		for j in range(0,len(X)):
 			value=numpy.power(X[j],i)*(estimateFun(X[j],theta,order)-Y[j])
 #			print estimateFun(X[j],theta,order)-Y[j]
@@ -72,8 +70,9 @@ def updateTheta(theta,delta,X,Y,order,a):
 		delta[i]=sum
 		theta[i]=theta[i]-a*sum
 
-X,Y = genData(10)
-ax.plot(X,Y,color='r',linestyle='',marker='.')
+X,Y,tmp = generateData(100)
+ax.plot(X,Y,color='r',linestyle='',marker='.')#画出加入噪声后的数据
+ax.plot(X,tmp,color='black',linestyle='-',marker='')#画出正弦曲线
 count=0
 while True:
 	tmp1=lossFun(X,Y,theta, order)
@@ -83,8 +82,11 @@ while True:
 #	print theta
 	tmp2=lossFun(X,Y,theta, order)
 	count+=1
-	print (tmp1-tmp2)
+	print 'J=',tmp2,', count=',count,', delta=',abs(tmp2-tmp1)
 	if abs(tmp2-tmp1)<e:
+		break
+	if count>100000:
+		print '迭代次数过多，请修改参数'
 		break
 
 #画出拟合后的曲线
